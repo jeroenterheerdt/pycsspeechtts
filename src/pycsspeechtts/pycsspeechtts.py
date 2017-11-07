e198bbf5a27cc5c2fc402c60fde26c9fc756020c
jeroenterheerdt <jeroen_ter_heerdt@hotmail.com>
2017-11-07 10:46:57 +0100

    Adding support for speed, pitch, volume and contour.

--- src/pycsspeechtts/pycsspeechtts.py
+++ src/pycsspeechtts/pycsspeechtts.py
@@ -5,6 +5,7 @@ Python wrapper for Microsoft Cognitive Services Text-to-speech translator
 import http.client, urllib.parse, json
 from xml.etree import ElementTree
 import logging
+import sys
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,17 +31,26 @@ class TTSTranslator(object):
             data = response.read()
             self._accesstoken = data.decode("UTF-8")
         else:
-            _LOGGER.debug("Connection Intialization failed, statuscode "+str(response.status)+", reason: "+response.reason)
+            _LOGGER.error("Connection Intialization failed, statuscode "+str(response.status)+", reason: "+response.reason)
+            sys.exit(1)
         conn.close()
 
-    def speak(self, language="en-us", gender="Female", voiceType="ZiraRUS", output="riff-16khz-16bit-mono-pcm", text=None):
+    def speak(self, language="en-us", gender="Female", voiceType="ZiraRUS", output="riff-16khz-16bit-mono-pcm", rate="+0.00%", volume="+0.00%", pitch="default", contour="(0%,+0%) (100%,+0%)", text=None):
         body = ElementTree.Element('speak',version='1.0')
         body.set('{http://www.w3.org/XML/1998/namespace}lang',language)
+        
         voice = ElementTree.SubElement(body,'voice')
         voice.set('{http://www.w3.org/XML/1998/namespace}lang',language)
         voice.set('{http://www.w3.org/XML/1998/namespace}gender',gender)
         voice.set('name','Microsoft Server Speech Text to Speech Voice ('+language+', '+voiceType+')')
-        voice.text = text
+                
+        prosody = ElementTree.SubElement(voice,'prosody')
+        prosody.set('rate',rate)
+        prosody.set('volume',volume)
+        prosody.set('pitch', pitch)
+        prosody.set('contour',contour)
+        prosody.text = text
+
         headers = {"Content-Type": "application/ssml+xml",
                     "X-Microsoft-OutputFormat": output,
                     "Authorization": "Bearer " + self._accesstoken,
@@ -48,7 +58,7 @@ class TTSTranslator(object):
 			        "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
                     "User-Agent": "PYCSSpeechTTS"
         }
-
+        
         conn = http.client.HTTPSConnection(SpeechHost)
         conn.request("POST","/synthesize",ElementTree.tostring(body),headers)
         response = conn.getresponse()
@@ -57,6 +67,6 @@ class TTSTranslator(object):
             data = response.read()
             return data
         else:
-            _LOGGER.debug("Text synthesis failed, statuscode "+str(response.status)+", reason: "+response.reason)
+            _LOGGER.error("Text synthesis failed, statuscode "+str(response.status)+", reason: "+response.reason)
             return None
         conn.close()
--- src/pycsspeechtts/test.py
+++ src/pycsspeechtts/test.py
@@ -1,10 +1,19 @@
 from pycsspeechtts import TTSTranslator
 t = TTSTranslator("YOUR API KEY")
 
-
+#Speaking with default language of english US and default Female voice
 data = t.speak(text='This is a test')
+#Change speed with -50%
+data = t.speak(text="This is a test",rate="-50%")
+#Change pitch to high
+data = t.speak(text="This is a test",pitch="high")
+#Change volume to +20%
+data = t.speak(text="This is a test",volume="+20%")
+#Using contour to change pitch from normal at 0% of speech and +100% at 100% of speech
+data = t.speak(text="This is a test",contour="(0%,+0%) (100%,+100%)")
 #See https://docs.microsoft.com/en-us/azure/cognitive-services/Speech/api-reference-rest/bingvoiceoutput for the accepted values of the parameters below
 data = t.speak('en-gb','Male', 'George, Apollo', 'audio-16khz-128kbitrate-mono-mp3', 'I am Max')
+
 
 if data == None:
     print("An error occured")
