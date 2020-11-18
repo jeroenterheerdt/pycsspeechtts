19729c38274f40068a801f969aa8a5bd294e89e9
Jeroen ter Heerdt <jterh@microsoft.com>
2020-11-18 11:45:48 -0800

    moving setup.py to root instead of /src, in response to #12

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
-
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
 setup(name='pycsspeechtts',
-      version='1.0.2',
+      version='1.0.4',
       description='Python 3 interface to Microsoft Cognitive Services Text To Speech',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/jeroenterheerdt/pycsspeechtts',
       author='Jeroen ter Heerdt',
       license='MIT',
